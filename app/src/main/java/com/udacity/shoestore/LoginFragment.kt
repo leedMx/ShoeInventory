c92b44fd91c8d79f25c08806e45f32c19307121b
leedMx <esparza.mexico@gmail.com>
2021-06-09 03:24:54 -0500

    Implemented navigation from login to welcome
    Using data binding for setOnClickListener()

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -4,12 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
-        i: LayoutInflater, c: ViewGroup?, bundle: Bundle?): View? {
-        return i.inflate(R.layout.fragment_login, c, false)
+        i: LayoutInflater, c: ViewGroup?, bundle: Bundle?): View {
+        binding = DataBindingUtil.inflate(
+            i, R.layout.fragment_login, c, false)
+        val listener = Listener()
+        binding.loginButton.setOnClickListener(listener)
+        binding.signupButton.setOnClickListener(listener)
+        return binding.root
+    }
+
+    class Listener() : View.OnClickListener{
+        override fun onClick(v: View) {
+            v.findNavController().navigate(
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
     }
 }
