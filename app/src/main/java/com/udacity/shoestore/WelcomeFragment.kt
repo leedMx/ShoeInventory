173966de9616c53f887b7bf79a864823b5024e51
leedMx <esparza.mexico@gmail.com>
2021-06-09 04:05:29 -0500

    Implemented navigation towards InstructionsFragment.kt
    Using a different coding style for setting the on click listener just for the sake of example

--- app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+    private lateinit var binding: FragmentWelcomeBinding
     override fun onCreateView(
         li: LayoutInflater, vg: ViewGroup?, bundle: Bundle?
-    ): View? {
-        return li.inflate(R.layout.fragment_welcome, vg, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+            li, R.layout.fragment_welcome, vg, false)
+        setListener()
+        return binding.root
+    }
+
+    private fun setListener() {
+        binding.instructionsButton.setOnClickListener {
+            it.findNavController().navigate(
+                WelcomeFragmentDirections
+                    .actionWelcomeFragmentToInstructionsFragment()
+            )
+        }
     }
 }
