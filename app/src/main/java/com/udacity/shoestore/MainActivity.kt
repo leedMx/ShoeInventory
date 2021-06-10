1b55e3e54861e7c60274021844d7d27b6676bef2
leedMx <esparza.mexico@gmail.com>
2021-06-10 01:33:20 -0500

    Set up nav controller to use AppBarConfiguration
    Learned that nav_host_fragment MUST be just a fragment.
    And that the layout for an activity must have a container in order to hold a fragment.

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navigation: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.plant(Timber.DebugTree())
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        navigation = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navigation)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navigation.navigateUp()
+        return super.onSupportNavigateUp()
     }
 }
